d63c0c24b5b8cf7e76628cc1def6efed66ce824c
Lucas Santos <lucas.parkl@outlook.com>
2022-12-25 16:49:22 -0300

    feat: utilizando Logbook para retornar log Json: - adicionando comentários para cada tipo de log do Logbook

--- java--spring-with-secret-manager/src/main/java/com/lucassilvs/javaspringwithsecretmanager/config/LogbookConfiguration.java
+++ java--spring-with-secret-manager/src/main/java/com/lucassilvs/javaspringwithsecretmanager/config/LogbookConfiguration.java
@@ -2,7 +2,6 @@ package com.lucassilvs.javaspringwithsecretmanager.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.zalando.logbook.CurlHttpLogFormatter;
 import org.zalando.logbook.DefaultHttpLogWriter;
 import org.zalando.logbook.DefaultSink;
 import org.zalando.logbook.Logbook;
@@ -15,7 +14,8 @@ public class LogbookConfiguration {
     public Logbook logbook() {
         Logbook logbook = Logbook.builder()
 //                .sink(new CommonsLogFormatSink(new DefaultHttpLogWriter(), ZoneId.systemDefault())) Log HTTPS, exemplo: 0:0:0:0:0:0:0:1 - - [25/dez./2022:16:08:13 -0300] "GET /usuario?cpf=23634689889 HTTP/1.1" 200 118
-                .sink(new DefaultSink(new CurlHttpLogFormatter(new JsonHttpLogFormatter()), new DefaultHttpLogWriter()))
+//                .sink(new DefaultSink(new CurlHttpLogFormatter(new JsonHttpLogFormatter()), new DefaultHttpLogWriter())) Log Request em formato de CURL
+                .sink(new DefaultSink(new JsonHttpLogFormatter(), new DefaultHttpLogWriter())) // Log em formato JSON no request e response
                 .build();
         return logbook;
     }
